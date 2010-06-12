87ef8a87f47ea48dd664986e22b9ef622b58b52f
lexi <lexi@6ed13606-9e1b-ccdd-a29f-aa03f5ca764e>
2010-06-12 11:14:08 +0000

    JIIB-14
    git-svn-id: https://svn.java.net/svn/jaxb2-commons~svn/trunk/basics@107 6ed13606-9e1b-ccdd-a29f-aa03f5ca764e

---
+++ basic/src/main/java/org/jvnet/jaxb2_commons/plugin/autoinheritance/AutoInheritancePlugin.java
@@ -0,0 +1,116 @@
+package org.jvnet.jaxb2_commons.plugin.autoinheritance;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jvnet.jaxb2_commons.plugin.AbstractParameterizablePlugin;
+import org.xml.sax.ErrorHandler;
+
+import com.sun.codemodel.JClass;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.tools.xjc.Options;
+import com.sun.tools.xjc.outline.ClassOutline;
+import com.sun.tools.xjc.outline.Outline;
+
+public class AutoInheritancePlugin extends AbstractParameterizablePlugin {
+
+	private String globalElementsExtend = null;
+	private List<String> globalElementsImplement = new LinkedList<String>();
+
+	private String globalComplexTypesExtend = null;
+	private List<String> globalComplexTypesImplement = new LinkedList<String>();
+
+	public String getGlobalElementsExtend() {
+		return globalElementsExtend;
+	}
+
+	public void setGlobalElementsExtend(String globalElementsExtend) {
+		this.globalElementsExtend = globalElementsExtend;
+	}
+
+	public String getGlobalElementsImplement() {
+		return globalElementsImplement.toString();
+	}
+
+	public void setGlobalElementsImplement(String globalElementsImplement) {
+		this.globalElementsImplement.add(globalElementsImplement);
+	}
+
+	public String getGlobalComplexTypesExtend() {
+		return globalComplexTypesExtend;
+	}
+
+	public void setGlobalComplexTypesExtend(String globalComplexTypesExtend) {
+		this.globalComplexTypesExtend = globalComplexTypesExtend;
+	}
+
+	public String getGlobalComplexTypesImplement() {
+		return globalComplexTypesImplement.toString();
+	}
+
+	public void setGlobalComplexTypesImplement(
+			String globalComplexTypesImplement) {
+		this.globalComplexTypesImplement.add(globalComplexTypesImplement);
+	}
+
+	@Override
+	public String getOptionName() {
+		return "XautoInheritance";
+	}
+
+	@Override
+	public String getUsage() {
+		return "TBD";
+	}
+
+	@Override
+	public boolean run(Outline outline, Options opt, ErrorHandler errorHandler) {
+		for (final ClassOutline classOutline : outline.getClasses()) {
+			if (classOutline.target.getElementName() != null) {
+				processGlobalElement(classOutline);
+			} else {
+				processGlobalComplexType(classOutline);
+			}
+		}
+		return true;
+	}
+
+	protected void processGlobalElement(ClassOutline classOutline) {
+
+		generateExtends(classOutline.implClass, globalElementsExtend);
+		generateImplements(classOutline.implClass, globalElementsImplement);
+
+	}
+
+	protected void processGlobalComplexType(ClassOutline classOutline) {
+
+		generateExtends(classOutline.implClass, globalComplexTypesExtend);
+		generateImplements(classOutline.implClass, globalComplexTypesImplement);
+
+	}
+
+	private void generateExtends(JDefinedClass theClass, String name) {
+		if (name != null) {
+			final JClass targetClass = theClass.owner().ref(name);
+			if (theClass._extends() == theClass.owner().ref(Object.class)) {
+				theClass._extends(targetClass);
+			}
+		}
+	}
+
+	private void generateImplements(JDefinedClass theClass, String name) {
+		if (name != null) {
+			final JClass targetClass = theClass.owner().ref(name);
+			theClass._implements(targetClass);
+		}
+	}
+
+	private void generateImplements(JDefinedClass theClass, List<String> names) {
+		if (names != null && !names.isEmpty()) {
+			for (String name : names) {
+				generateImplements(theClass, name);
+			}
+		}
+	}
+
+}
